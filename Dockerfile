1c42e3b2c0a8eec36bbcea6641dc125a4c2a5196
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-06-02 15:59:06 +0200

    Remove Docker configuration and clean dependencies

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:14-alpine
-WORKDIR /usr/src/app
-COPY package.json pnpm-lock.yaml ./
-RUN npm install -g pnpm && pnpm install
-COPY . .
-EXPOSE 4200
-CMD ["pnpm", "start"]
-
--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,22 @@
-// @ts-check
+// @ts-nocheck
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
-  
+
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
     extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      "plugin:@angular-eslint/recommended",
+      "plugin:@angular-eslint/template/process-inline-templates",
+      "plugin:prettier/recommended",
     ],
-    processor: angular.processInlineTemplates,
+    parserOptions: {
+      project: "./tsconfig.json",
+    },
     rules: {
       "@angular-eslint/directive-selector": [
         "error",
@@ -30,14 +34,19 @@ module.exports = tseslint.config(
           style: "kebab-case",
         },
       ],
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
   {
     files: ["**/*.html"],
     extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
+      "plugin:@angular-eslint/template/recommended",
+      "plugin:prettier/recommended",
     ],
-    rules: {},
+    rules: {
+      "max-len": ["error", { code: 140 }],
+    },
   }
 );
